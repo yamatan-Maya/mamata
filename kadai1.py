3ba4c93854f71135786079917acd4799e5bfff61
yamatan-Maya <mamatagrg9906@gmail.com>
2026-05-07 06:34:17 +0000

    good

---
+++ kadai1.py
@@ -0,0 +1,9 @@
+score=int(input("Enter your score : "))
+if score >= 60:
+    print("合格")
+    print("Congratulations!")
+else:
+     print("Fail")
+     print("Try again")
+
+
---
+++ kadai2.py
@@ -0,0 +1,5 @@
+name = input("Enter your name:")
+age = input("age:")
+print(f"Hello {name}")
+print(f"you are{age}")
+
